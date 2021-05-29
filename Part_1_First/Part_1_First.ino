538ca9186b866bac182353e258d53858f40463d2
AFraij <ahmad-fraij@hotmail.com>
2021-05-29 18:09:38 +0300

    Update Part_1_First.ino
    Added the scrolling text on the screen bonus feature

--- Part_1_First/Part_1_First.ino
+++ Part_1_First/Part_1_First.ino
@@ -2,15 +2,45 @@
 
 #include <FastLED.h>
 
+//Libraries needed for scrolling text
+#include <Adafruit_GFX.h>
+#include <Adafruit_NeoMatrix.h>
+#include <Adafruit_NeoPixel.h>
+
+#ifndef PSTR
+ #define PSTR // Make Arduino Due happy
+#endif
+
+#define PIN 5
+
+//Declare matrix needed to display information
+//First two parameters give width and height of the matrix
+//Fourth parameter gives the matrix layout
+Adafruit_NeoMatrix Mode_Info = Adafruit_NeoMatrix(5, 5, PIN,
+  NEO_MATRIX_TOP     + NEO_MATRIX_LEFT +
+ NEO_MATRIX_COLUMNS + NEO_MATRIX_PROGRESSIVE,
+  NEO_KHZ800           + NEO_GRB );
+
+//define an array for displaying colors on the screen
+const uint16_t colors[] = {
+  Mode_Info.Color(255, 0, 0), Mode_Info.Color(0, 255, 0), Mode_Info.Color(0, 0, 255) };
 
 void setup() {
   // put your setup code here, to run once:
   // Initialising the device
   M5.begin(true, false, true);
+  //initializing the matrix for displaying info
+  Mode_Info.begin();
+  Mode_Info.setTextWrap(false);
+  Mode_Info.setBrightness(20);
+  Mode_Info.setTextColor(colors[0]);
   delay(50);
 }
 
 uint8_t case_code = 0; // Denoting the current case of the display
+
+int z    = Mode_Info.width();
+int y = 0;
 
 void loop() {
   // put your main code here, to run repeatedly:
@@ -23,9 +53,24 @@ void loop() {
           for (;M5.Btn.wasPressed();)
           {
             M5.dis.fillpix(0xff0000); // Strobe with red colour
+           
             delay(200);
             M5.dis.clear();
             delay(200);
+
+            Mode_Info.fillScreen(0);
+            Mode_Info.setCursor(z, 0);
+             Mode_Info.print(F("MANUAL REAR STROBE"));
+             if(--z < -36) {
+              z = Mode_Info.width();
+              if(++y >= 3){ 
+              y = 0;
+              }
+             Mode_Info.setTextColor(colors[y]);
+             }
+             Mode_Info.show();
+           
+             delay(100);
           }
           break;
 
@@ -33,8 +78,21 @@ void loop() {
           for (;M5.Btn.wasPressed();)
           {
             M5.dis.fillpix(0xffffff); // Strobe with white colour
+            
             delay(200);
             M5.dis.clear();
+
+             Mode_Info.fillScreen(0);
+            Mode_Info.setCursor(z, 0);
+             Mode_Info.print(F("MANUAL FRONT STROBE"));
+             if(--z < -36) {
+              z = Mode_Info.width();
+              if(++y >= 3){ 
+              y = 0;
+              }
+             Mode_Info.setTextColor(colors[y]);
+             }
+             Mode_Info.show();
             delay(200);
           }
           break;
@@ -62,6 +120,6 @@ void loop() {
       {
         case_code = 0;
       }
-    }
+    } 
   }
 }
