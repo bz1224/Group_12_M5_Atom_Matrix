292e67c461d2512dc157c199351b9ed04a44d968
AFraij <ahmad-fraij@hotmail.com>
2021-05-31 00:43:44 +0300

    Update PART_2.ino
    Fixed some bugs

--- PART_2/PART_2.ino
+++ PART_2/PART_2.ino
@@ -1,4 +1,6 @@
-#include <M5Stack.h>
+//#include <M5Stack.h>
+#include <M5Atom.h>
+#include <FastLED.h>
 
 float AvgAccX = 0.0F;
 float AvgAccY = 0.0F;
@@ -13,7 +15,8 @@ void setup(){
 
   //start with a black screen
 
-  fillScreen(cBLACK);
+  M5.dis.fillpix(0x0000);
+
 }
 
 // the loop routine runs over and over again forever
@@ -22,10 +25,11 @@ void loop() {
 
     float accX = 0, accY = 0, accZ = 0;
   M5.IMU.getAccelData(&accX, &accY, &accZ);
-  // Averaging the acceleration data to determine when the object tilts
-  accZ_avg = ((accZ_avg * (n_average-1))+accZ)/n_average;
-  accY_avg = ((accY_avg * (n_average-1))+accY)/n_average;
-  accX_avg = ((accX_avg * (n_average-1))+accX)/n_average;
+  int n_average=15;
+  // Averaging 15 different acceleration data to determine when the object tilts
+  AvgAccZ = ((AvgAccZ * (n_average-1))+accZ)/n_average;
+  AvgAccY = ((AvgAccY * (n_average-1))+accY)/n_average;
+  AvgAccX = ((AvgAccX * (n_average-1))+accX)/n_average;
 
   if (M5.Btn.wasPressed()){
 
@@ -33,7 +37,7 @@ void loop() {
     //Write the code needed for showing the temperature when the button is pressed
     
 
-    if(accZ_avg > 0.7 && abs(accX_avg) > 0.3){ // 0.3g = 0.27 deg tilt angle (perform the tasks below if the angle is greater than 0.27 degrees left or right
+    if(AvgAccZ > 0.7 && abs(AvgAccX) > 0.3){ // 0.3g = 0.27 deg tilt angle (perform the tasks below if the angle is greater than 0.27 degrees left or right
 
         //show the code needed to change game mode
       }
@@ -47,9 +51,4 @@ void loop() {
 
   M5.update();
 }
-}
-
-  
-
- 
 }
