b1ebf4c5f041b49779f447d2073b59d0cc6b5e7f
AFraij <ahmad-fraij@hotmail.com>
2021-06-01 13:39:16 +0300

    Update Part_1_First_Most_Complete.ino
    Fixed the millis() function

--- Part_1_First_200ms_delay/Part_1_First_Most_Complete/Part_1_First_Most_Complete.ino
+++ Part_1_First_200ms_delay/Part_1_First_Most_Complete/Part_1_First_Most_Complete.ino
@@ -4,31 +4,34 @@
 
 #include <TimeLib.h>
 
+#define interval1 400
+#define interval2 200
 
-//Libraries needed for scrolling text
-#include <Adafruit_GFX.h>
-#include <Adafruit_NeoMatrix.h>
-#include <Adafruit_NeoPixel.h>
+/*
+  //Libraries needed for scrolling text
+  #include <Adafruit_GFX.h>
+  #include <Adafruit_NeoMatrix.h>
+  #include <Adafruit_NeoPixel.h>
 
-#ifndef PSTR
-#define PSTR // Make Arduino Due happy
-#endif
+  #ifndef PSTR
+  #define PSTR // Make Arduino Due happy
+  #endif
 
-#define PiN 5
+  #define PiN 5
 
-//Declare matrix needed to display information
-//First two parameters give width and height of the matrix
-//Fourth parameter gives the matrix layout
-Adafruit_NeoMatrix Mode_Info = Adafruit_NeoMatrix(5, 5, PiN,
+  //Declare matrix needed to display information
+  //First two parameters give width and height of the matrix
+  //Fourth parameter gives the matrix layout
+  Adafruit_NeoMatrix Mode_Info = Adafruit_NeoMatrix(5, 5, PiN,
                                NEO_MATRIX_TOP     + NEO_MATRIX_LEFT +
                                NEO_MATRIX_COLUMNS + NEO_MATRIX_PROGRESSIVE,
                                NEO_KHZ800           + NEO_GRB );
 
-//define an array for displaying colors on the screen
-const uint16_t colors[] = {
+  //define an array for displaying colors on the screen
+  const uint16_t colors[] = {
   Mode_Info.Color(255, 0, 0), Mode_Info.Color(0, 255, 0), Mode_Info.Color(0, 0, 255)
-};
-
+  };
+*/
 // Inplementing Interrupt Service Routine for detecting button pressed during strobe
 // Reference is from https://lastminuteengineers.com/handling-esp32-gpio-interrupts-tutorial/
 
@@ -58,20 +61,23 @@ void setup() {
   attachInterrupt(main_btn.PIN, isr, FALLING); // Attaching the ISR to the button
 
   //initializing the matrix for displaying info
-  Mode_Info.begin();
-  Mode_Info.setTextWrap(false);
-  Mode_Info.setBrightness(20);
-  Mode_Info.setTextColor(colors[0]);
+  /*
+    Mode_Info.begin();
+    Mode_Info.setTextWrap(false);
+    Mode_Info.setBrightness(20);
+    Mode_Info.setTextColor(colors[0]);
+  */
 }
-
-int z = Mode_Info.width();
-int y = 0;
+/*
+  int z = Mode_Info.width();
+  int y = 0;
+*/
 unsigned long CurrentTime;
+unsigned long PreviousTime;
 
 // Detecting constantly for pressed button, change the case accordingly.
 void loop() {
 
-  CurrentTime = millis();
 
   float accX, accY, accZ;
 
@@ -82,7 +88,10 @@ void loop() {
 
       for (;;)
       {
+
         M5.dis.fillpix(0x000000); // Keeping the LEDs black
+
+
         if (main_btn.pressed) {
           main_btn.pressed = false;
           break;
@@ -91,35 +100,34 @@ void loop() {
 
     case 1:
 
-      M5.dis.fillpix(0xff0000); // Strobe with red colour
+
       for (;;)
       {
-
-
-        if (millis() >= CurrentTime + 200) {
-          M5.dis.clear();
-          CurrentTime = millis();
+        CurrentTime = millis();
+        if (CurrentTime - PreviousTime >= interval1) {
+          M5.dis.fillpix(0xff0000); // Strobe with red colour
+          PreviousTime = millis();
         }
-
-        if (millis() >= CurrentTime + 100 && millis() < CurrentTime + 200) {
-          M5.dis.fillpix(0xff0000);
+        if (CurrentTime - PreviousTime >= interval2) {
+          M5.dis.clear();
         }
 
 
         //code needed for scrolling text
-        Mode_Info.fillScreen(0);
-        Mode_Info.setCursor(z, 0);
-        Mode_Info.print(F("MANUAL REAR STROBE"));
-        if (--z < -36) {
+        /*
+          Mode_Info.fillScreen(0);
+          Mode_Info.setCursor(z, 0);
+          Mode_Info.print(F("MANUAL REAR STROBE"));
+          if (--z < -36) {
           z = Mode_Info.width();
           if (++y >= 3) {
             y = 0;
           }
           Mode_Info.setTextColor(colors[y]);
-        }
-        Mode_Info.show();
+          }
+          Mode_Info.show();
 
-
+        */
 
         if (main_btn.pressed) {
           main_btn.pressed = false;
@@ -130,30 +138,32 @@ void loop() {
 
     case 2:
 
-      M5.dis.fillpix(0xffffff); // Strobe with white colour
+
       for (;;)
       {
-        if (millis() >= CurrentTime + 200) {
+        CurrentTime = millis();
+        if (CurrentTime - PreviousTime >= interval1) {
+         M5.dis.fillpix(0xffffff); // Strobe with white colour
+          PreviousTime = millis();
+        }
+        if (CurrentTime - PreviousTime >= interval2) {
           M5.dis.clear();
-          CurrentTime = millis();
         }
+       
+        /*
 
-        if (millis() >= CurrentTime + 100 && millis() < CurrentTime + 200) {
-          M5.dis.fillpix(0xff0000);
-        }
-
-        Mode_Info.fillScreen(0);
-        Mode_Info.setCursor(z, 0);
-        Mode_Info.print(F("MANUAL FRONT STROBE"));
-        if (--z < -36) {
+          Mode_Info.fillScreen(0);
+          Mode_Info.setCursor(z, 0);
+          Mode_Info.print(F("MANUAL FRONT STROBE"));
+          if (--z < -36) {
           z = Mode_Info.width();
           if (++y >= 3) {
             y = 0;
           }
           Mode_Info.setTextColor(colors[y]);
-        }
-        Mode_Info.show();
-
+          }
+          Mode_Info.show();
+        */
         if (main_btn.pressed) {
           main_btn.pressed = false;
           break;
@@ -170,30 +180,31 @@ void loop() {
       M5.dis.fillpix(0xff0000); // Strobe with red colour
       for (;;)
       {
-        Mode_Info.fillScreen(0);
-        Mode_Info.setCursor(z, 0);
-        Mode_Info.print(F("AUTOMATIC REAR MODE"));
-        if (--z < -36) {
+        /*
+          Mode_Info.fillScreen(0);
+          Mode_Info.setCursor(z, 0);
+          Mode_Info.print(F("AUTOMATIC REAR MODE"));
+          if (--z < -36) {
           z = Mode_Info.width();
           if (++y >= 3) {
             y = 0;
           }
           Mode_Info.setTextColor(colors[y]);
-        }
-        Mode_Info.show();
-
+          }
+          Mode_Info.show();
+        */
         M5.IMU.getAccelData(&accX, &accY, &accZ);
         // Taking the 3D acceleration as a vector and calculating the square of its magnitude
         if (accX * accX + accY * accY + accZ * accZ > 6.25) {
           continue;
         }
         else {
-          CurrentTime = millis();
+          PreviousTime = millis();
           M5.dis.clear();
           for (;;) {
-            if (millis() >= CurrentTime + 200) {
+            CurrentTime=millis();
+            if (CurrentTime-PreviousTime >= interval1) {
               M5.dis.fillpix(0xff0000);
-              CurrentTime = millis();
               break;
             }
           }
@@ -211,30 +222,31 @@ void loop() {
       M5.dis.fillpix(0xffffff); // Strobe with white colour
       for (;;)
       {
-        Mode_Info.fillScreen(0);
-        Mode_Info.setCursor(z, 0);
-        Mode_Info.print(F("AUTOMATIC FRONT MODE"));
-        if (--z < -36) {
+        /*
+          Mode_Info.fillScreen(0);
+          Mode_Info.setCursor(z, 0);
+          Mode_Info.print(F("AUTOMATIC FRONT MODE"));
+          if (--z < -36) {
           z = Mode_Info.width();
           if (++y >= 3) {
             y = 0;
           }
           Mode_Info.setTextColor(colors[y]);
-        }
-        Mode_Info.show();
-
+          }
+          Mode_Info.show();
+        */
         M5.IMU.getAccelData(&accX, &accY, &accZ);
         // Taking the 3D acceleration as a vector and calculating the square of its magnitude
         if (accX * accX + accY * accY + accZ * accZ > 6.25) {
           continue;
         }
         else {
-          CurrentTime = millis();
+          PreviousTime = millis();
           M5.dis.clear();
           for (;;) {
-            if (millis() >= CurrentTime + 200) {
+            CurrentTime=millis();
+            if (CurrentTime-PreviousTime>=interval1) {
               M5.dis.fillpix(0xffffff);
-              CurrentTime = millis();
               break;
             }
           }
