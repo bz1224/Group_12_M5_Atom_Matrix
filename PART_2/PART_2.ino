22c378c4f8921600ca700a9b96af9e150eb9d299
AFraij <ahmad-fraij@hotmail.com>
2021-06-03 11:35:27 +0300

    Fixed the tilt function

--- PART_2/PART_2.ino
+++ PART_2/PART_2.ino
@@ -16,7 +16,11 @@
 //Declare matrix needed to display information
 //First two parameters give width and height of the matrix
 //Fourth parameter gives the matrix layout
+<<<<<<< Updated upstream
 Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(5,5, PiN,
+=======
+Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(5, 5, PiN,
+>>>>>>> Stashed changes
                             NEO_MATRIX_TOP    + NEO_MATRIX_RIGHT +
                             NEO_MATRIX_COLUMNS + NEO_MATRIX_PROGRESSIVE,
                             NEO_GRB          + NEO_KHZ800  );
@@ -27,6 +31,11 @@ float accX = 0, accY = 0, accZ = 0;
 float gyroX = 0, gyroY = 0, gyroZ = 0;
 float temp = 0;
 float AvgTemp = 0;
+<<<<<<< Updated upstream
+=======
+int ButtonPressedAtLeastOnce = 0;
+bool case_activated = false;
+>>>>>>> Stashed changes
 
 float AvgAccX = 0.0F;
 float AvgAccY = 0.0F;
@@ -51,7 +60,11 @@ void setup()
   //initializing the matrix for displaying info
   matrix.begin();
   matrix.setTextWrap(false);
+<<<<<<< Updated upstream
  
+=======
+
+>>>>>>> Stashed changes
   matrix.setBrightness(20);
   matrix.setTextColor(colors[0]);
 }
@@ -63,7 +76,11 @@ int case_code = 0;
 
 void loop()
 {
+<<<<<<< Updated upstream
   
+=======
+  matrix.setTextColor(colors[0]);
+>>>>>>> Stashed changes
   M5.IMU.getGyroData(&gyroX, &gyroY, &gyroZ);
   M5.IMU.getAccelData(&accX, &accY, &accZ);
   M5.IMU.getTempData(&temp);
@@ -79,13 +96,23 @@ void loop()
 
 
 
+<<<<<<< Updated upstream
   if (M5.Btn.wasPressed() ) {
+=======
+  if (M5.Btn.wasPressed() || ButtonPressedAtLeastOnce == 1 ) {
+    ButtonPressedAtLeastOnce = 1;
+>>>>>>> Stashed changes
 
     switch (case_code) {
 
       case 0:
         for (;;) {
 
+<<<<<<< Updated upstream
+=======
+
+
+>>>>>>> Stashed changes
           CurrentTime = millis();
 
           M5.IMU.getGyroData(&gyroX, &gyroY, &gyroZ);
@@ -101,6 +128,7 @@ void loop()
           AvgAccY = ((AvgAccY * (n_average - 1)) + accY) / n_average;
           AvgAccX = ((AvgAccX * (n_average - 1)) + accX) / n_average;
 
+<<<<<<< Updated upstream
 
           if (CurrentTime - PreviousTime > 200) {
             PreviousTime = millis();
@@ -138,6 +166,48 @@ void loop()
 
         }
       
+=======
+          if (abs(AvgAccX) < 0.4 || case_activated == true) {
+
+            case_activated = true;
+
+            if (CurrentTime - PreviousTime > 200) {
+              PreviousTime = millis();
+              matrix.fillScreen(0);
+              matrix.setCursor(z, 0);
+              matrix.printf("TEMPERATURE : %.2f ", temp);
+              if (--z < -106) {
+                z = matrix.width();
+                if (++y >= 3) {
+                  y = 0;
+                }
+                matrix.setTextColor(colors[y]);
+              }
+              matrix.show();
+
+            }
+
+
+
+            if (AvgAccX > 0.4) { //tilting to the right
+              case_code += 1;
+
+              break;
+
+              //show the code needed to change  mode
+            }
+
+
+            else if ( AvgAccX < -0.4) {
+              case_code = 4;
+              break;
+            }
+
+
+          }
+        }
+        case_activated = false;
+>>>>>>> Stashed changes
         break;
 
 
@@ -150,19 +220,30 @@ void loop()
 
         for (;;) {
 
+<<<<<<< Updated upstream
           CurrentTime = millis();
           
+=======
+
+          CurrentTime = millis();
+
+>>>>>>> Stashed changes
           M5.IMU.getGyroData(&gyroX, &gyroY, &gyroZ);
           M5.IMU.getAccelData(&accX, &accY, &accZ);
           M5.IMU.getTempData(&temp);
 
+<<<<<<< Updated upstream
          
+=======
+
+>>>>>>> Stashed changes
           int n_average = 15;
           // Averaging 15 different acceleration data to determine when the object tilts
           AvgAccZ = ((AvgAccZ * (n_average - 1)) + accZ) / n_average;
           AvgAccY = ((AvgAccY * (n_average - 1)) + accY) / n_average;
           AvgAccX = ((AvgAccX * (n_average - 1)) + accX) / n_average;
 
+<<<<<<< Updated upstream
           if (CurrentTime - PreviousTime > 200) { // 0.3g = 27deg tilt angle
             M5.IMU.getGyroData(&gyroX, &gyroY, &gyroZ);
             matrix.fillScreen(0);
@@ -199,6 +280,49 @@ void loop()
         }
         
         
+=======
+          if (abs(AvgAccX) < 0.4 || case_activated == true) {
+            case_activated = true;
+
+
+            if (CurrentTime - PreviousTime > 200) { // 0.3g = 27deg tilt angle
+              M5.IMU.getGyroData(&gyroX, &gyroY, &gyroZ);
+              matrix.fillScreen(0);
+              matrix.setCursor(z, 0);
+              matrix.printf("AVERAGE TEMPERATURE IS : % .2f C", AvgTemp);
+              if (--z < -106) {
+                z = matrix.width();
+                if (++y >= 3) {
+                  y = 0;
+                }
+                matrix.setTextColor(colors[y]);
+              }
+              matrix.show();
+              PreviousTime = millis();
+
+            }
+
+            if (AvgAccX > 0.4) { //tilting to the right
+              case_code += 1;
+              break;
+
+
+
+            }
+
+
+            else if ( AvgAccX < -0.4) {
+              case_code -= 1;
+
+              break;
+            }
+          }
+        }
+        case_activated = false;
+        break;
+
+
+>>>>>>> Stashed changes
       case 2:
 
         for (;;) {
@@ -214,8 +338,14 @@ void loop()
           AvgAccZ = ((AvgAccZ * (n_average - 1)) + accZ) / n_average;
           AvgAccY = ((AvgAccY * (n_average - 1)) + accY) / n_average;
           AvgAccX = ((AvgAccX * (n_average - 1)) + accX) / n_average;
+<<<<<<< Updated upstream
 
           if (AvgAccZ < 0.7 && abs(AvgAccX) < 0.3) { // 0.3g = 27deg tilt angle
+=======
+          if (abs(AvgAccX) < 0.4 || case_activated == true) {
+            case_activated = true;
+
+>>>>>>> Stashed changes
             M5.dis.drawpix(0, 0, 0x0000ff); // blue
             M5.dis.drawpix(0, 1, 0x87cefa); // skyblue
             M5.dis.drawpix(0, 2, 0x008000); // green
@@ -223,48 +353,80 @@ void loop()
             M5.dis.drawpix(0, 4, 0xff0000); // red
 
             if (temp < 25) {
+<<<<<<< Updated upstream
               for (int i = 2; i < 5; i++){
                 for (int j = 1; j < 4; j++){
+=======
+              for (int i = 2; i < 5; i++) {
+                for (int j = 1; j < 4; j++) {
+>>>>>>> Stashed changes
                   M5.dis.drawpix(i, j, 0x0000ff);
                 }
               }
             }
 
             else if (temp >= 25 && temp < 30) {
+<<<<<<< Updated upstream
               for (int i = 2; i < 5; i++){
                 for (int j = 1; j < 4; j++){
+=======
+              for (int i = 2; i < 5; i++) {
+                for (int j = 1; j < 4; j++) {
+>>>>>>> Stashed changes
                   M5.dis.drawpix(i, j, 0x87cefa);
                 }
               }
             }
 
             else if (temp >= 30 && temp < 35) {
+<<<<<<< Updated upstream
               for (int i = 2; i < 5; i++){
                 for (int j = 1; j < 4; j++){
+=======
+              for (int i = 2; i < 5; i++) {
+                for (int j = 1; j < 4; j++) {
+>>>>>>> Stashed changes
                   M5.dis.drawpix(i, j, 0x008000);
                 }
               }
             }
 
             else if (temp >= 35 && temp < 40) {
+<<<<<<< Updated upstream
               for (int i = 2; i < 5; i++){
                 for (int j = 1; j < 4; j++){
+=======
+              for (int i = 2; i < 5; i++) {
+                for (int j = 1; j < 4; j++) {
+>>>>>>> Stashed changes
                   M5.dis.drawpix(i, j, 0xffff00);
                 }
               }
             }
 
             else {
+<<<<<<< Updated upstream
               for (int i = 2; i < 5; i++){
                 for (int j = 1; j < 4; j++){
+=======
+              for (int i = 2; i < 5; i++) {
+                for (int j = 1; j < 4; j++) {
+>>>>>>> Stashed changes
                   M5.dis.drawpix(i, j, 0xff0000);
                 }
               }
             }
+<<<<<<< Updated upstream
           }
           M5.clear();
 
           if (AvgAccX > 0.5) { //tilting to the right
+=======
+        
+          //M5.clear();
+          matrix.setTextColor(colors[0]);
+          if (AvgAccX > 0.4) { //tilting to the right
+>>>>>>> Stashed changes
             case_code += 1;
             break;
 
@@ -272,6 +434,7 @@ void loop()
           }
 
 
+<<<<<<< Updated upstream
           else if (AvgAccX < -0.5) {
             case_code -= 1;
             break;
@@ -457,3 +620,136 @@ void loop() {
   delay(200);
 }
 */
+=======
+          else if (AvgAccX < -0.4) {
+            case_code -= 1;
+            break;
+          }
+        }
+    }
+    case_activated = false;
+    break;
+
+
+  case 3:
+    for (;;) {
+      CurrentTime = millis();
+      M5.IMU.getGyroData(&gyroX, &gyroY, &gyroZ);
+      M5.IMU.getAccelData(&accX, &accY, &accZ);
+      M5.IMU.getTempData(&temp);
+
+      Serial.printf("%.2f,%.2f,%.2f o/s \r\n", gyroX, gyroY, gyroZ);
+      Serial.printf("%.2f,%.2f,%.2f mg\r\n", accX * 1000, accY * 1000, accZ * 1000);
+
+      int n_average = 15;
+      // Averaging 15 different acceleration data to determine when the object tilts
+      AvgAccZ = ((AvgAccZ * (n_average - 1)) + accZ) / n_average;
+      AvgAccY = ((AvgAccY * (n_average - 1)) + accY) / n_average;
+      AvgAccX = ((AvgAccX * (n_average - 1)) + accX) / n_average;
+
+      if (abs(AvgAccX) < 0.4 || case_activated == true) {
+        case_activated = true;
+        if (CurrentTime - PreviousTime > 200) {
+          M5.IMU.getGyroData(&gyroX, &gyroY, &gyroZ);
+          matrix.fillScreen(0);
+          matrix.setCursor(z, 0);
+          matrix.printf("Mode 4");
+          if (--z < -106) {
+            z = matrix.width();
+            if (++y >= 3) {
+              y = 0;
+            }
+            matrix.setTextColor(colors[y]);
+          }
+          matrix.show();
+          PreviousTime = millis();
+        }
+
+
+        if (AvgAccX > 0.4) { //tilting to the right
+          case_code += 1;
+          break;
+
+
+        }
+
+
+        else if (AvgAccX < -0.4) {
+          case_code -= 1;
+          break;
+        }
+      }
+    }
+    case_activated = false;
+    break;
+
+
+  case 4:
+    for (;;) {
+      CurrentTime = millis();
+      M5.IMU.getGyroData(&gyroX, &gyroY, &gyroZ);
+      M5.IMU.getAccelData(&accX, &accY, &accZ);
+      M5.IMU.getTempData(&temp);
+
+      Serial.printf("%.2f,%.2f,%.2f o/s \r\n", gyroX, gyroY, gyroZ);
+      Serial.printf("%.2f,%.2f,%.2f mg\r\n", accX * 1000, accY * 1000, accZ * 1000);
+
+      int n_average = 15;
+      // Averaging 15 different acceleration data to determine when the object tilts
+      AvgAccZ = ((AvgAccZ * (n_average - 1)) + accZ) / n_average;
+      AvgAccY = ((AvgAccY * (n_average - 1)) + accY) / n_average;
+      AvgAccX = ((AvgAccX * (n_average - 1)) + accX) / n_average;
+
+      if (abs(AvgAccX) < 0.4 || case_activated == true) {
+        case_activated = true;
+        if (CurrentTime - PreviousTime > 200) {
+
+
+
+          M5.IMU.getGyroData(&gyroX, &gyroY, &gyroZ);
+          matrix.fillScreen(0);
+          matrix.setCursor(z, 0);
+          matrix.printf("Mode 5");
+          if (--z < -76) {
+            z = matrix.width();
+            if (++y >= 3) {
+              y = 0;
+            }
+            matrix.setTextColor(colors[y]);
+          }
+          matrix.show();
+          PreviousTime = millis();
+        }
+
+        if (AvgAccX > 0.4) { //tilting to the right
+          case_code = 0;
+          break;
+
+
+        }
+
+
+        else if (AvgAccX < -0.4) {
+          case_code -= 1;
+          break;
+        }
+
+      }
+    }
+    case_activated = false;
+    break;
+
+  default:
+    break;
+
+
+  }
+
+}
+
+
+
+M5.update();
+
+}
+>>>>>>> Stashed changes
