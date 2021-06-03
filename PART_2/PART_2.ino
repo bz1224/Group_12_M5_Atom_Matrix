781731147af0087c823e49bc2ed1cdd75fe87187
AFraij <ahmad-fraij@hotmail.com>
2021-06-03 14:23:56 +0300

    Update PART_2.ino

--- PART_2/PART_2.ino
+++ PART_2/PART_2.ino
@@ -22,13 +22,12 @@
 Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(5, 5, PiN,
 
                             NEO_MATRIX_TOP    + NEO_MATRIX_RIGHT +
-                            NEO_MATRIX_COLUMNS + NEO_MATRIX_PROGRESSIVE,
+                            NEO_MATRIX_ROWS + NEO_MATRIX_PROGRESSIVE,
                             NEO_GRB          + NEO_KHZ800  );
 
 
 
 float accX = 0, accY = 0, accZ = 0;
-float gyroX = 0, gyroY = 0, gyroZ = 0;
 float temp = 0;
 float AvgTemp = 0;
 
@@ -72,14 +71,12 @@ int case_code = 0;
 void loop()
 {
 
-  matrix.setTextColor(colors[0]);
+  M5.dis.drawpix(0, 0xf000000);
 
-  M5.IMU.getGyroData(&gyroX, &gyroY, &gyroZ);
   M5.IMU.getAccelData(&accX, &accY, &accZ);
   M5.IMU.getTempData(&temp);
 
-  Serial.printf("%.2f,%.2f,%.2f o/s \r\n", gyroX, gyroY, gyroZ);
-  Serial.printf("%.2f,%.2f,%.2f mg\r\n", accX * 1000, accY * 1000, accZ * 1000);
+
 
   int n_average = 15;
   // Averaging 15 different acceleration data to determine when the object tilts
@@ -107,11 +104,11 @@ void loop()
 
           CurrentTime = millis();
 
-          M5.IMU.getGyroData(&gyroX, &gyroY, &gyroZ);
+    
           M5.IMU.getAccelData(&accX, &accY, &accZ);
           M5.IMU.getTempData(&temp);
 
-          Serial.printf("%.2f,%.2f,%.2f o/s \r\n", gyroX, gyroY, gyroZ);
+     
           Serial.printf("%.2f,%.2f,%.2f mg\r\n", accX * 1000, accY * 1000, accZ * 1000);
 
           int n_average = 15;
@@ -178,7 +175,7 @@ void loop()
           CurrentTime = millis();
 
 
-          M5.IMU.getGyroData(&gyroX, &gyroY, &gyroZ);
+      
           M5.IMU.getAccelData(&accX, &accY, &accZ);
           M5.IMU.getTempData(&temp);
 
@@ -195,7 +192,7 @@ void loop()
 
 
             if (CurrentTime - PreviousTime > 200) { // 0.3g = 27deg tilt angle
-              M5.IMU.getGyroData(&gyroX, &gyroY, &gyroZ);
+              
               matrix.fillScreen(0);
               matrix.setCursor(z, 0);
               matrix.printf("AVERAGE TEMPERATURE IS : % .2f C", AvgTemp);
@@ -239,7 +236,7 @@ void loop()
           M5.IMU.getAccelData(&accX, &accY, &accZ);
           M5.IMU.getTempData(&temp);
 
-          Serial.printf("%.2f,%.2f,%.2f o/s \r\n", gyroX, gyroY, gyroZ);
+          
           Serial.printf("%.2f,%.2f,%.2f mg\r\n", accX * 1000, accY * 1000, accZ * 1000);
 
           int n_average = 15;
@@ -315,7 +312,7 @@ void loop()
         
         
           //M5.clear();
-          matrix.setTextColor(colors[0]);
+           M5.dis.drawpix(0, 0xf000000);
           if (AvgAccX > 0.4) { //tilting to the right
 
             case_code += 1;
@@ -339,7 +336,7 @@ void loop()
 case 3:
     for (;;) {
       CurrentTime = millis();
-      M5.IMU.getGyroData(&gyroX, &gyroY, &gyroZ);
+      
       M5.IMU.getAccelData(&accX, &accY, &accZ);
       M5.IMU.getTempData(&temp);
 
@@ -355,7 +352,7 @@ case 3:
       if (abs(AvgAccX) < 0.4 || case_activated == true) {
         case_activated = true;
         if (CurrentTime - PreviousTime > 200) {
-          M5.IMU.getGyroData(&gyroX, &gyroY, &gyroZ);
+          
           matrix.fillScreen(0);
           matrix.setCursor(z, 0);
           matrix.printf("Mode 4");
@@ -392,12 +389,12 @@ case 3:
       case 4:
     for (;;) {
       CurrentTime = millis();
-      M5.IMU.getGyroData(&gyroX, &gyroY, &gyroZ);
+
       M5.IMU.getAccelData(&accX, &accY, &accZ);
       M5.IMU.getTempData(&temp);
 
-      Serial.printf("%.2f,%.2f,%.2f o/s \r\n", gyroX, gyroY, gyroZ);
-      Serial.printf("%.2f,%.2f,%.2f mg\r\n", accX * 1000, accY * 1000, accZ * 1000);
+     
+   
 
       int n_average = 15;
       // Averaging 15 different acceleration data to determine when the object tilts
@@ -411,7 +408,7 @@ case 3:
 
 
 
-          M5.IMU.getGyroData(&gyroX, &gyroY, &gyroZ);
+          
           matrix.fillScreen(0);
           matrix.setCursor(z, 0);
           matrix.printf("Mode 5");
